37b870fbc1e90e11d658651054f88979fd5044fd
root <root@arkho.me>
2018-08-21 03:35:30 +0000

    ER

---
+++ clean.sh
@@ -0,0 +1,14 @@
+sed -i -e 's/http\:\/\/fromthemachine.org/\./g' *.html
+sed -i -e 's/https\:\/\/fromthemachine.org/\./g' *.html
+sed -i -e 's/http\:\/\/lamc.la/\./g' *.html
+sed -i -e 's/https\:\/\/lamc.la/\./g' *.html
+
+sed -i -e 's/http\:\/\/www.lamc.la/\./g' *.html
+sed -i -e 's/https\:\/\/www.lamc.la/\./g' *.html
+
+sed -i -e 's/http\:\/\/en.reallyhim.com/\./g' *.html
+sed -i -e 's/https\:\/\/en.reallyhim.com/\./g' *.html
+
+sed -i -e 's/http\:\/\/i.imgur.com/\.\/i.imgur.com/g' *.html
+sed -i -e 's/https\:\/\/i.imgur.com/\.\/i.imgur.com/g' *.html
+
